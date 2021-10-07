179cb712bf6deac12ece55ccfdad5aea6a62b152
unknown <truongvanhao159@gmail.com>
2021-10-07 21:53:26 +0700

    js login

--- js.js
+++ js.js
@@ -13,3 +13,32 @@ function countFollow() {
         divEle.textContent = "Đã theo dõi";
     }
 }
+
+
+function loginAccount() {
+    var loginEmail = document.getElementById("input-email").value;
+    var passEmail = document.getElementById("input-password").value;
+    if(loginEmail && passEmail)
+    {
+        var failLogin = document.querySelector('.fail-login');
+        Object.assign(failLogin.style, {
+            display: 'none',
+        })
+        var noneUser = document.querySelector('.header__navbar-user');
+        Object.assign(noneUser.style, {
+            display: 'none',
+        })
+        var nameAccount = document.querySelector('.login-name');
+        nameAccount.textContent = loginEmail;
+        var loggedEle = document.querySelector('.header__navbar-user-logged');
+        Object.assign(loggedEle.style, {
+            display: 'flex',
+        })
+    }
+    else {
+        var failLogin = document.querySelector('.fail-login');
+        Object.assign(failLogin.style, {
+            display: 'block',
+        })
+    }
+}
